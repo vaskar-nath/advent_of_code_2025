15f59157fd83f3c182ae63caf06baa0293793bb6
Vaskar Nath <vaskarnath2000@gmail.com>
2025-12-14 22:23:51 -0500

    added time check + quick experiment on the order of chunk processing

--- day_two/solution.py
+++ day_two/solution.py
@@ -12,16 +12,19 @@ def _find_all_invalid_ids(start, end):
             ret += int(si)
     return ret
 
-def _find_all_invalid_ids_two(start, end):
+def _find_all_invalid_ids_two(start, end, r):
     ret = 0
     for i in range(start, end + 1):
         si = str(i)
         l = len(si)
-        for k in range(1, l):
+
+        range_exp = range(l-1, 0, -1) if r else range(1, l)
+        for k in range_exp:
             if l % k == 0:
                 is_not_valid = True
                 start_entry = None
-                for t in range(0, l//k):
+                range_exp_two = range(l//k - 1, -1, -1) if r else range(0, l//k)
+                for t in range_exp_two:
                     curr_entry = si[t*k:(t+1)*k]
                     if start_entry == None:
                         start_entry = curr_entry
@@ -42,15 +45,24 @@ def solution_one():
         ans += _find_all_invalid_ids(int(entry[0]), int(entry[1]))
     return ans
 
-def solution_two():
+def solution_two(r):
 
     ans = 0
     for entry in _read_input():
-        ans += _find_all_invalid_ids_two(int(entry[0]), int(entry[1]))
+        ans += _find_all_invalid_ids_two(int(entry[0]), int(entry[1]), r)
     return ans
     
 
 
 if __name__ == "__main__":
+    import time
     print(f"{solution_one()=}")
-    print(f"{solution_two()=}")
+    s = time.time()
+    print(f"{solution_two(r=False)=}")
+    e = time.time()
+    print(f"Took {e - s} seconds")
+
+    s = time.time()
+    print(f"{solution_two(r=True)=}")
+    e = time.time()
+    print(f"Took {e - s} seconds")
